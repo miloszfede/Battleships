5b65e846633a498ff8677925dc4574e5d65d8b09
CodeMeNawh <89380204+CodeMeNawh@users.noreply.github.com>
2024-07-30 19:16:05 +0200

    hello there

--- TestAppp/Battleship.cs
+++ TestAppp/Battleship.cs
@@ -89,8 +89,7 @@ namespace TestAppp
             {
             int numberOfShips = 5;
             int shipsPlaced = 0;
-            Player p1 = new Player("PLAYER 1", 1);
-            Player p2 = new Player("PLAYER 2", 2);
+            
             while (shipsPlaced < numberOfShips)
 
             {
@@ -129,7 +128,7 @@ namespace TestAppp
                     Console.WriteLine($"Number of ships before adding: {placedShips.Count}");
                     Console.WriteLine($"{shipType} placed successfully.");
                     shipsPlaced++;
-                    p1.Fleet.Add(ship); //tu wyskakuje błąd! "Object reference not set to an instance of an object."
+                    
 
 
 
